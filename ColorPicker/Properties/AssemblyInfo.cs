bf096ec9749789fe52b47257343abe33b0f69cab
Stephen Workman <workman.stephen@gmail.com>
2020-02-22 17:47:00 +0000

    Add license info.

--- ColorPicker/Properties/AssemblyInfo.cs
+++ ColorPicker/Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ColorPicker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("WPF Color Picker")]
+[assembly: AssemblyDescription("A set of basic colour picking controls for WPF.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ColorPicker")]
+[assembly: AssemblyCompany("mrworkman")]
+[assembly: AssemblyProduct("WPF Color Picker")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +49,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
+
+// Major.Minor.Revision
+[assembly: AssemblyInformationalVersion("0.1.0")]
