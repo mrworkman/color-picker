56b4d68966bf3dd25211004dea81d31cda44f7ba
Stephen Workman <workman.stephen@gmail.com>
2020-08-12 16:40:56 +0100

    WIP

---
+++ ColorPicker/ColorComboBox.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MrWorkman.Wpf {
+   public class ColorComboBox : ComboBox {
+      static ColorComboBox() {
+         DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorComboBox), new FrameworkPropertyMetadata(typeof(ColorComboBox)));
+      }
+
+      public static readonly DependencyProperty IsPickerDropDownOpenProperty = DependencyProperty.Register(
+         "IsPickerDropDownOpen", typeof(bool), typeof(ColorComboBox), new FrameworkPropertyMetadata(
+            false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault
+         )
+      );
+
+      [Bindable(true)]
+      public bool IsPickerDropDownOpen {
+         get => (bool) GetValue(IsPickerDropDownOpenProperty);
+         set => SetValue(IsPickerDropDownOpenProperty, value);
+      }
+
+
+
+   }
+}
